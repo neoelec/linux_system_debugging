e7aa10a8ab2d1e14d74c2d8c5763d84848eda69b
YOUNGJIN JOO <neoelec@gmail.com>
2025-10-19 07:55:13 +0000

    update makefiles to enabel UBSAN
    Signed-off-by: YOUNGJIN JOO <neoelec@gmail.com>

--- prj_nasm.mk
+++ prj_nasm.mk
@@ -1,14 +1,12 @@
-# SPDX-License-Identifier: GPL-2.0+
-# Copyright (c) 2024 YOUNGJIN JOO (neoelec@gmail.com)
-
 PRJ_NASM_MK_FILE	:= $(realpath $(lastword $(MAKEFILE_LIST)))
 PRJ_NASM_MK_DIR		:= $(shell dirname $(PRJ_NASM_MK_FILE))
+
+VPATH			+= $(PRJ_NATIVE_MK_DIR)/asm_io
+EXTRAINCDIRS		+= $(PRJ_NATIVE_MK_DIR)/asm_io
 
 CSRCS			+= asm_io_c.c asm_glue_c.c
 ASRCS			+= asm_io.asm asm_glue.asm
 
-VPATH			+= $(PRJ_NATIVE_MK_DIR)/asm_io
-EXTRAINCDIRS		+= $(PRJ_NATIVE_MK_DIR)/asm_io
 LDFLAGS			+= -Wl,-z execstack
 
 include $(PRJ_NASM_MK_DIR)/prj_native.mk
--- prj_native.mk
+++ prj_native.mk
@@ -1,15 +1,20 @@
-# SPDX-License-Identifier: GPL-2.0+
-# Copyright (c) 2024 YOUNGJIN JOO (neoelec@gmail.com)
-
 PRJ_NATIVE_MK_FILE	:= $(realpath $(lastword $(MAKEFILE_LIST)))
 PRJ_NATIVE_MK_DIR	:= $(shell dirname $(PRJ_NATIVE_MK_FILE))
 
 I_HDD00			:= $(HOME)/iHDD00
 MK_RACCOON_DIR		:= $(I_HDD00)/08.PROJECT/mk-raccoon
 
-OPT			:= 0
-CFLAGS			+= -Wall -Werror -Wextra -Wno-unused-parameter
+CFLAGS			+= -Wextra
+CFLAGS			+= -Wno-unused-parameter
+CFLAGS			+= -Wno-unused-result
+CFLAGS			+= -fsanitize=undefined
+CFLAGS			+= -fno-sanitize-recover=all
+
+LDFLAGS			+= -lubsan
+
+OPT			:= g
 
 AS_MK			:= $(MK_RACCOON_DIR)/mk/as_nasm.mk
 
 include $(MK_RACCOON_DIR)/gcc_native.mk
+# include $(MK_RACCOON_DIR)/llvm_native.mk
