bd0cb2d9277ff884dde30c2cbda1b6e209ed872b
YOUNGJIN JOO <neoelec@gmail.com>
2026-01-04 03:11:55 +0900

    chapter_08: update Makefile
    Signed-off-by: YOUNGJIN JOO <neoelec@gmail.com>

--- chapter_08/ptrace.mk
+++ chapter_08/ptrace.mk
@@ -1,7 +1,15 @@
-PTRACE_PNAME		?= hello1
+TARGET_COMM		?= hello1
+TARGET_PID		:= $(shell ps -ef | grep -E "\b$(TARGET_COMM)\b" | sed -e '/grep/d' | awk '{print $$2}')
 
 ptrace: $(OUTPUT)
-	@sudo $(OUTPUT) \
-		`ps -ef  | grep $(PTRACE_PNAME) | sed -e '/grep/d' | awk '{print $$2}'`
+	@if [ -z "$(TARGET_PID)" ]; then \
+		echo "[\033[31mFAIL\033[0m] run $(TARGET_COMM) before testing..."; \
+		exit; \
+	else \
+		sudo $(OUTPUT) $(TARGET_PID) $(TARGET_ADDR); \
+	fi
+
+kill:
+	@if [ -n "$(TARGET_PID)" ]; then kill -9 $(TARGET_PID); fi
 
 .PHONY: ptrace
